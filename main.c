c67ec690333daffd1ae1cae53e65376abea6e33f
1915157 <chonsa0409@naver.com>
2019-11-19 15:34:18 +0900

    4

--- main.c
+++ main.c
@@ -4,13 +4,10 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 void main(void){
+
+	char str[30] = "happy C programming";
 	
-	char src[] = "The worst things to eat before you sleep";
-	char dst[100];
-	
-	strcpy(dst, src);
-	
-	printf("copied string : %s", dst);
+	printf("string \"%s\" length : %i", str, strlen(str) );
 	
 	return;
 }
