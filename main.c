8dac36456f91ca51841c5372287b096e30bda4f0
1915157 <chonsa0409@naver.com>
2019-11-26 16:31:54 +0900

    12 total

--- main.c
+++ main.c
@@ -1,3 +1,68 @@
+// 12-02>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	
+	int i = 0;
+	char str[4];
+	str[0] = 'a';
+	str[1] = 'b';
+	str[2] = 'c';
+	str[3] = '\0';
+	
+	printf("%s\n", str);
+	
+	while (str[i] != '\0')
+	{
+			
+		printf("%c", str[i]);
+		i++;
+	}
+	
+	return;
+}
+
+// 12 - 03>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main(void){
+	
+	char src[] = "The worst things to eat before you sleep";
+	char dst[100];
+	
+	strcpy(dst, src);
+	
+	printf("copied string : %s", dst);
+	
+	return;
+}
+
+// 12 - 04>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main(void){
+
+	char str[30] = "happy C programming";
+	
+	printf("string \"%s\" length : %i", str, strlen(str) );
+	
+	return;
+}
+
+// 12 - 05 >
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
